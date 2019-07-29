05b9c2f08d3008558fc2d5d7aa5a0e32e0580083
Danilo <dl539994@gmail.com>
2019-07-29 15:29:42 -0300

    Finalizar Jogo da Velha

---
+++ readme.md
@@ -0,0 +1,23 @@
+#JOGO DA VELHA!
+
+##linguagens Que Foram Utilizadas:
+
+- JAVASCRIPT;
+- HTML;
+- CSS.
+
+###Ferramentas das Linguagens:
+
+~~~
+- Utilizamos em `JAVASCRIPT`: For, Function, If, Else If e Else;
+- Utilizamos em `CSS`: Display Flex, Justify-content, e outras propriedades simples de edição;
+
+**EX DO JS:  
+function resetarJogo(){
+     for(let quadrado of quadrados){
+         quadrado.innerHTML="";
+     }
+     resposta.innerHTML = "jogo reset";
+ }
+
+ botao.onclick = resetarJogo;
--- script.js
+++ script.js
@@ -1,38 +1,65 @@
 let quadrados = document.querySelectorAll("div");
 let i = 0;
-
+let botao = document.querySelector("button")
+let resposta = document.querySelector("h2")
 function jogada(){
     if(this.innerHTML == ""){
         if(i % 2 == 0){
-            this.innerHTML = "x"    
-            this.style.color = "white";
+            this.innerHTML = "X"   
+             
+            this.style.color = "black";
             i++;
+            verificarResultado();
         } else {
-            this.innerHTML = "o";
-            this.style.color = "white";
+            this.innerHTML = "O";
+            this.style.color = "black";
             i++;
+            verificarResultado();
         }
     }
 }
 
-for (let quadrado of quadrados) {
+
+
+for(let quadrado of quadrados){
     quadrado.onclick = jogada;
 }
-
-
-
-function verificarFinal(){
-    if(quadrados[0].style.none == quadrados[1].style.backgroundColor && quadrados[1].style.backgroundColor == quadrados[2].style.backgroundColor && quadrados[2].style.backgroundColor == quadrados[3].style.backgroundColor){
-        resposta.innerHTML = "!!! VOCÊ VENCEU !!!";
-        
-        for(let quadrado of quadrados){
-            quadrado.onclick = null;
-            setTimeout(() => {
-                quadrado.classList.add("virarQuadrado");
-            }, 1000);
-        }  
+function verificarResultado (){
+    if(quadrados[0].innerHTML !=="" && quadrados[0].innerHTML === quadrados[1].innerHTML && quadrados[1].innerHTML=== quadrados[2].innerHTML){
+        resposta.innerHTML = "você venceu!";
+    } 
+    else if(quadrados[3].innerHTML !=="" && quadrados[3].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[5].innerHTML){
+        resposta.innerHTML = "você venceu!";
+    } 
+    else if(quadrados[6].innerHTML !=="" && quadrados[6].innerHTML === quadrados[7].innerHTML && quadrados[7].innerHTML=== quadrados[8].innerHTML){
+        resposta.innerHTML = "você venceu!";
+    } 
+    else if(quadrados[0].innerHTML !=="" && quadrados[0].innerHTML === quadrados[3].innerHTML && quadrados[3].innerHTML=== quadrados[6].innerHTML){
+        resposta.innerHTML = "você venceu!";
+    } 
+    else if(quadrados[1].innerHTML !=="" && quadrados[1].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[7].innerHTML){
+        resposta.innerHTML = "você venceu!";
+    } 
+    else if(quadrados[2].innerHTML !=="" && quadrados[2].innerHTML === quadrados[5].innerHTML && quadrados[5].innerHTML=== quadrados[8].innerHTML){
+        resposta.innerHTML = "você venceu!";
+    } 
+    else if(quadrados[0].innerHTML !=="" && quadrados[0].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[8].innerHTML){
+        resposta.innerHTML = "você venceu!";
+    } 
+    else if(quadrados[2].innerHTML !=="" && quadrados[2].innerHTML === quadrados[4].innerHTML && quadrados[4].innerHTML=== quadrados[6].innerHTML){
+        resposta.innerHTML = "você venceu!";
+    } 
+    else if(i==9){
+        resposta.innerHTML="deu velha";
     }
-    else{
-        resposta.innerHTML= "Ainda não foi !!";
-    }
+
 }
+
+ function resetarJogo(){
+     for(let quadrado of quadrados){
+         quadrado.innerHTML="";
+     }
+     resposta.innerHTML = "jogo reset";
+ }
+
+ botao.onclick = resetarJogo;
